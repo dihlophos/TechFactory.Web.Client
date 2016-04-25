5698b4cde2ab3f6f3d3a18eb4b2066d9410e2bb1
dihlophos <dihlophos2@gmail.com>
2016-04-25 17:23:00 +0300

    Async controls disabling

---
+++ scripts/products/orders.list.controller.js
@@ -0,0 +1,22 @@
+﻿angular.module('tfApp').controller('ordersListController', function (ordersFactory, $scope) {
+
+    $scope.formatDate = formatDate;
+
+    updateScope();
+
+    function updateScope() {
+        ordersFactory.get()
+            .then(function (answer) {
+                $scope.orders = answer.data.value;
+            }, onError);
+    };
+
+    function formatDate(date) {
+        return new Date(date).toString('f');
+    }
+
+
+    function onError(answer) {
+        alert("Error. Status: " + answer.status + "; StatusText: " + answer.statusText);
+    };
+});
