ab6a5617d0b15060e35a30d14ae124e36d7e1ae0
dihlophos <dihlophos2@gmail.com>
2016-05-05 16:42:15 +0300

    More backend constants

--- scripts/config.js
+++ scripts/config.js
@@ -8,7 +8,7 @@ angular.module('tfApp').config(['$routeProvider', function ($routeProvider) {
 
 .constant('backendProperties', {
     baseUrl: "http://partner-dev-srv.cloudapp.net/v1/odata/",
-    productsCollection: baseUrl + "Products/?$expand=Price",
-    orderLinesCollectionUrl: baseUrl + "/OrderLines",
-    ordersCollectionUrl: baseUrl + "/Orders"
+    productsCollection: this.baseUrl + "Products/?$expand=Price",
+    orderLinesCollectionUrl: this.baseUrl + "/OrderLines",
+    ordersCollectionUrl: this.baseUrl + "/Orders"
 });
