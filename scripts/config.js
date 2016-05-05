1d7e595000a22c65c7dc3d08e8cd62a0dbf0a7ed
dihlophos <dihlophos2@gmail.com>
2016-05-05 18:15:14 +0300

    CategoriesTree

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,7 @@
 angular.module('tfApp').config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when('/', {   templateUrl: 'pages/productsList.html', controller: 'productsListController', reloadOnSearch: false});
-  $routeProvider.when('/products', {   templateUrl: 'pages/productsList.html', controller: 'productsListController', reloadOnSearch: false});
+  $routeProvider.when('/', { templateUrl: 'pages/categoriesList.html', controller: 'categoriesListController', reloadOnSearch: false });
+  $routeProvider.when('/categories/:id', { templateUrl: 'pages/categoriesList.html', controller: 'categoriesListController', reloadOnSearch: false });
+  $routeProvider.when('/products/:id', {   templateUrl: 'pages/productsList.html', controller: 'productsListController', reloadOnSearch: false});
   $routeProvider.when('/orders', {   templateUrl: 'pages/ordersList.html', controller: 'ordersListController', reloadOnSearch: false});
   $routeProvider.when('/order/:id', {   templateUrl: 'pages/ordersForm.html', controller: 'ordersFormController', reloadOnSearch: false});
   $routeProvider.otherwise('/');
@@ -9,5 +10,7 @@ angular.module('tfApp').config(['$routeProvider', function ($routeProvider) {
 .constant('backendProperties', {
     productsCollectionUrl: "http://partner-dev-srv.cloudapp.net/v1/odata/Products/?$expand=Price",
     orderLinesCollectionUrl: "http://partner-dev-srv.cloudapp.net/v1/odata/OrderLines",
-    ordersCollectionUrl: "http://partner-dev-srv.cloudapp.net/v1/odata/Orders"
+    ordersCollectionUrl: "http://partner-dev-srv.cloudapp.net/v1/odata/Orders",
+    categoriesCollectionUrl: "http://partner-dev-srv.cloudapp.net/v1/odata/Categories",
+    rootCategory: "MENU",
 });
