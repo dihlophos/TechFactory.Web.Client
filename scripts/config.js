e040dbc2b63d8ad8b8495a7735cf750a00a6f19d
dihlophos <dihlophos2@gmail.com>
2016-05-05 16:33:37 +0300

    backendProperties

--- scripts/config.js
+++ scripts/config.js
@@ -4,4 +4,8 @@ angular.module('tfApp').config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/orders', {   templateUrl: 'pages/ordersList.html', controller: 'ordersListController', reloadOnSearch: false});
   $routeProvider.when('/order/:id', {   templateUrl: 'pages/ordersForm.html', controller: 'ordersFormController', reloadOnSearch: false});
   $routeProvider.otherwise('/');
-}]);
+}])
+
+.constant('backendProperties', {
+    baseUrl: "http://partner-dev-srv.cloudapp.net/v1/odata/"
+});
