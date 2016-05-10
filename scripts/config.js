6e30261475a04dc45f20dca31eb236c9460b7367
dihlophos <dihlophos2@gmail.com>
2016-05-10 12:36:40 +0300

    Orders routing

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,7 @@
 ﻿angular.module('tfApp').config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/menu/:id?/', { templateUrl: 'pages/menu.html', controller: 'menuController', reloadOnSearch: false });
+  $routeProvider.when('/orders/', { templateUrl: 'pages/orders/ordersList.html', controller: 'ordersListController', reloadOnSearch: false });		 +//  $routeProvider.when('/orders/', { templateUrl: 'pages/orders/ordersList.html', controller: 'ordersListController', reloadOnSearch: false });
+  $routeProvider.when('/order/:id/', { templateUrl: 'pages/orders/ordersForm.html', controller: 'ordersFormController', reloadOnSearch: false });
 //  $routeProvider.when('/categories/:id/', { templateUrl: 'pages/categories/categoriesList.html', controller: 'categoriesListController', reloadOnSearch: false });
 //  $routeProvider.when('/products/:catid/', { templateUrl: 'pages/products/productsList.html', controller: 'productsListController', reloadOnSearch: false });
 //  $routeProvider.when('/orders/', { templateUrl: 'pages/orders/ordersList.html', controller: 'ordersListController', reloadOnSearch: false });
