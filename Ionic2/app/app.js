6db6b44aa9ee602bad31a84fe1853f5b1d19e559
Sergey Dushkin <leirby@live.ru>
2016-05-29 11:38:53 +0300

    add category list page

--- Ionic2/app/app.js
+++ Ionic2/app/app.js
@@ -3,6 +3,7 @@ import {App, Platform, MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
 import {ListPage} from './pages/list/list';
+import {CategoryListPage} from './pages/category-list/category-list';
 
 
 @App({
@@ -25,7 +26,8 @@ class MyApp {
     // set our app's pages
     this.pages = [
       { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage }
+      { title: 'My First List', component: ListPage },
+      { title: 'Category List', component: CategoryListPage }
     ];
 
     // make HelloIonicPage the root (or first) page
