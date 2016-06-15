36ebe81b2557b72fc0ccca04c3ccd4ae1e626409
Xronus <robotxp@ya.ru>
2016-06-15 22:16:55 +0300

    add order

--- app/pages/orders/orders.ts
+++ app/pages/orders/orders.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
 import {NavController} from 'ionic-angular';
 import {Platform} from 'ionic-angular';
-
+import {OrderPage} from '../order/order';
 import {ApiService} from '../../services/api';
 
 @Component({
@@ -11,7 +11,15 @@ export class OrdersPage {
     order: string = "orders";
     isAndroid: boolean = false;
 
-    constructor(platform: Platform) {
+    constructor(
+        private navController: NavController,
+        private platform: Platform) {
         this.isAndroid = platform.is('android');
+    }
+
+    openOrderPage() {
+        this.navController.push(OrderPage, {
+        
+        });
     }
 }
