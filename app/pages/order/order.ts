03a2c39501c7c85514772ba40e09f41b9e2a4b14
Xronus <robotxp@ya.ru>
2016-06-18 20:50:12 +0300

    add sliding in category and bag fix

--- app/pages/order/order.ts
+++ app/pages/order/order.ts
@@ -10,44 +10,4 @@ export class OrderPage {
 
     constructor(public platform: Platform, public nav: NavController) { }
 
-    openMenu() {
-        let actionSheet = ActionSheet.create({
-            title: 'Options',
-            cssClass: 'order',
-            buttons: [
-                {
-                    text: 'Delete',
-                    role: 'destructive',
-                    icon: !this.platform.is('ios') ? 'trash' : null,
-                    handler: () => {
-                        console.log('Delete clicked');
-                    }
-                },
-                {
-                    text: 'Add',
-                    icon: !this.platform.is('ios') ? 'heart-outline' : null,
-                    handler: () => {
-                        console.log('Add clicked');
-                    }
-                },
-                {
-                    text: 'Take',
-                    icon: !this.platform.is('ios') ? 'heart-outline' : null,
-                    handler: () => {
-                        console.log('Take clicked');
-                    }
-                },
-                {
-                    text: 'Cancel',
-                    role: 'cancel', // will always sort to be on the bottom
-                    icon: !this.platform.is('ios') ? 'close' : null,
-                    handler: () => {
-                        console.log('Cancel clicked');
-                    }
-                }
-            ]
-        });
-
-        this.nav.present(actionSheet);
-    }
 }
